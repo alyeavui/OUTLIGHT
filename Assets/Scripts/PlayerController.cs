0ca2f8801d0067496cc01bd21c30b233662a9462
alyeavui <ayaulym.zhuniskhan89@gmail.com>
2025-11-20 01:11:48 +0500

    Added: Layers and UIs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,24 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 3f;
+    private Animator anim;
+    private float moveSpeed = 5f;
+    private float jumpForce = 5f;
     public InputActionReference moveAction;
     public InputActionReference jumpAction;
-    public bool isGrounded = true;
+    private bool isGrounded = true;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private bool faceRight = true;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     void OnEnable()
@@ -34,8 +38,28 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         moveInput = moveAction.action.ReadValue<Vector2>();
+        HandleAnimations();
+        HandleFlip();
     }
 
+    private void HandleAnimations()
+    {
+        bool isWalking = rb.linearVelocity.x != 0;
+        anim.SetBool("isWalking", isWalking);
+    }
+
+    private void HandleFlip()
+    {
+        if (rb.linearVelocity.x > 0 && faceRight == false)
+            Flip();
+        else if (rb.linearVelocity.x < 0 && faceRight == true)
+            Flip();
+    }
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        faceRight = !faceRight;
+    }
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
